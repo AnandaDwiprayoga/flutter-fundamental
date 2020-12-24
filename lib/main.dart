e5c898b7eec9c36f465780c410a15c806ba5b3e2
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2020-12-24 23:29:19 +0700

    unfinished project -skip

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:routing/FirstScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,20 +9,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Route',
+      title: 'Constrain',
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      //jika menggunakan initialRoute tidak bisa menggunakan parameter home
       initialRoute: '/',
       routes: {
-        '/': (context) => FirstScreen(),
-        '/secondScreen' : (context) => SecondScreen(),
-        '/secondScreenWithData' : (context) => SecondScreenWithData(),
-        '/returnDataScreen' : (context) => ReturnDataScreen(),
-        '/replacementScreen' : (context) => ReplacementScreen(),
-        '/anotherScreen' : (context) => AnotherScreen()
+        '/' : (context) => MainMenu(),
+        ConstrainBehavior.CONSTRAINT_BEHAVIOR_ROUTE : (context) => ConstrainBehavior()
       },
+    );
+  }
+}
+
+
+class MainMenu extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Constraint'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: (){
+                Navigator.pushNamed(context, ConstrainBehavior.CONSTRAINT_BEHAVIOR_ROUTE);
+              }, 
+              child: Text('Constraint behavior')
+            ),
+            FlatButton(
+              onPressed: (){
+                Navigator.pushNamed(context, ConstrainBehavior.CONSTRAINT_BEHAVIOR_ROUTE);
+              }, 
+              child: Text('Constraint behavior')
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ConstrainBehavior extends StatelessWidget {
+  static const CONSTRAINT_BEHAVIOR_ROUTE = "constraint behavior route";
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      //container akan memiliki tinggi yang sama dengan childnya jika height dan width tidak didefinisikan
+      //jika container tidak memiliki child dan tidak memiliki property height dan width maka lebarnya selayar
+      child: Container(
+        color: Colors.red,
+        padding: const EdgeInsets.all(10),
+        child: Container(
+          color: Colors.green,
+          width: 100,
+          height: 100,
+          child: FlatButton(
+            onPressed: () => Navigator.pop(context), 
+            child: Text('Back')
+          ),
+        ),
+      ),
     );
   }
 }
